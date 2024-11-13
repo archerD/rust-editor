dc9d2ab236f0a7c035bda7f355ec426a394c6bd4
David Flores <dflores0818@gmail.com>
2024-11-13 01:17:18 -0700

    Setting up flake for initial development

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Developing text editor in rust";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    naersk.url = "github:nix-community/naersk/master";
+  };
+
+  outputs = { self, nixpkgs, naersk }@inputs:
+  let 
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+    naersk-lib = pkgs.callPackage naersk { };
+  in {
+    packages.${system}.default = naersk-lib.buildPackage ./.; # source code is here.
+
+    devShells.${system}.default = pkgs.mkShell {
+      # CARGO_INSTALL_ROOT = "${toString ./.}/.cargo";
+      # RUST_SRC_PATH = rustPlatform.rustLibSrc;
+
+      buildInputs = with pkgs; [ cargo rustc rust-analyzer rustfmt rustPackages.clippy ];
+    };
+  };
+}
