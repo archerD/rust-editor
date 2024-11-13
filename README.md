93c4f7edc4e88d1d7b43dd395e7183cd8f72e9de
David Flores <dflores0818@gmail.com>
2024-11-13 01:52:53 -0700

    Adding gitignore, readme, and minor update to nix flake (style and systems)

---
+++ README.md
@@ -0,0 +1,7 @@
+# Hecto text editor
+
+Creating rust text editor, following (at least roughly) tutorial on [flenker.blog](archive.flenker.blog/hecto)
+
+## building/nix
+Setup to build and run using nix flakes: `nix run` should be sufficent on a system with nix installed and flakes enabled.
+
--- flake.nix
+++ flake.nix
@@ -3,21 +3,37 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     naersk.url = "github:nix-community/naersk/master";
+    utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, naersk }@inputs:
-  let 
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-    naersk-lib = pkgs.callPackage naersk { };
-  in {
-    packages.${system}.default = naersk-lib.buildPackage ./hecto; # source code is here.
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+      naersk,
+    }@inputs:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        naersk-lib = pkgs.callPackage naersk { };
+      in
+      {
+        packages.default = naersk-lib.buildPackage ./hecto; # source code is here.
 
-    devShells.${system}.default = pkgs.mkShell {
-      # CARGO_INSTALL_ROOT = "${toString ./.}/.cargo";
-      # RUST_SRC_PATH = rustPlatform.rustLibSrc;
+        devShells.default = pkgs.mkShell {
+          # CARGO_INSTALL_ROOT = "${toString ./.}/.cargo";
+          # RUST_SRC_PATH = rustPlatform.rustLibSrc;
 
-      buildInputs = with pkgs; [ cargo rustc rust-analyzer rustfmt rustPackages.clippy ];
-    };
-  };
+          buildInputs = with pkgs; [
+            cargo
+            rustc
+            rust-analyzer
+            rustfmt
+            rustPackages.clippy
+          ];
+        };
+      }
+    );
 }
