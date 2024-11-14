cd4d46c7dc158719ffd866b606a6ae28a133c3c8
David Flores <dflores0818@gmail.com>
2024-11-13 23:16:45 -0700

    hecto chapter 2, canonical mode

--- hecto/src/main.rs
+++ hecto/src/main.rs
@@ -1,3 +1,12 @@
+use std::io::{self, Read};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Enter characters");
+    for b in io::stdin().bytes() {
+        let c = b.unwrap() as char;
+        println!("{}", c);
+        if c == 'q' {
+            break;
+        }
+    }
 }
