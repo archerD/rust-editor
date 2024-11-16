ebb1aa5af8447df0e186a90b0c5dadfd536ec783
David Flores <dflores0818@gmail.com>
2024-11-16 03:49:14 -0700

    hecto chapter 3, more refactoring and display stuff and welcome message

--- hecto/src/editor.rs
+++ hecto/src/editor.rs
@@ -1,24 +1,22 @@
-use core::time;
-use std::io::{self, stdout, Write};
+use crate::Terminal;
 use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 pub struct Editor {
     should_quit: bool,
+    terminal: Terminal,
 }
 
 impl Editor {
     pub fn default() -> Self {
-        Self {should_quit: false}
+        Self {
+            should_quit: false,
+            terminal: Terminal::default().expect("Failed to initialize terminal"),
+        }
     }
 
     pub fn run(&mut self) {
-        // command raw mode
-        let _stdout = stdout().into_raw_mode().unwrap();
-
-        println!("Welcome to Hecto (DEF version)\r");
-        std::thread::sleep(time::Duration::from_secs(1)); // allows user to see message above
         loop {
             if let Err(e) = self.refresh_screen() {
                 die(e);
@@ -35,24 +33,44 @@ impl Editor {
     }
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
-        print!("{}{}", termion::clear::All, termion::cursor::Goto(1,1));
+        Terminal::cursor_hide();
+        Terminal::cursor_position(0, 0);
         if self.should_quit {
+            Terminal::clear_screen();
             println!("Goodbye!\r");
         } else {
             self.draw_rows();
-            print!("{}", termion::cursor::Goto(1,1));
+            Terminal::cursor_position(0, 0);
         }
-        io::stdout().flush()
+        Terminal::cursor_show();
+        Terminal::flush()
     }
 
     fn draw_rows(&self) {
-        for _ in 0..24 {
-            println!("~\r");
+        let height = self.terminal.size().height;
+        for row in 0..height - 1 {
+            Terminal::clear_current_line();
+            if row == height/3 {
+                self.draw_welcome_message();
+            } else {
+                println!("~\r");
+            }
         }
     }
 
+    fn draw_welcome_message(&self) {
+        let mut welcome_message = format!("Hecto editor (DEF edition) -- version {VERSION}\r");
+        let width = self.terminal.size().width as usize;
+        let len = welcome_message.len();
+        let padding = width.saturating_sub(len)/2;
+        let spaces = " ".repeat(padding.saturating_sub(1));
+        welcome_message = format!("~{spaces}{welcome_message}");
+        welcome_message.truncate(width);
+        println!("{welcome_message}\r");
+    }
+
     fn process_keypress(&mut self) -> Result<(), std::io::Error> {
-        let pressed_key = read_key()?;
+        let pressed_key = Terminal::read_key()?;
         match pressed_key {
             Key::Char(c) => {
                 println!("{c}\r");
@@ -64,15 +82,7 @@ impl Editor {
     }
 }
 
-fn read_key() -> Result<Key, std::io::Error> {
-    loop {
-        if let Some(key) = io::stdin().lock().keys().next() {
-            return key;
-        }
-    }
-}
-
 fn die(e: std::io::Error) {
-    print!("{}", termion::clear::All);
+    Terminal::clear_screen();
     panic!("{}", e);
 }
