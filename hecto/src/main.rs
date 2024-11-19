39a93f94b3c20025af2e2552009b6200c7b8bb44
David Flores <dflores0818@gmail.com>
2024-11-18 23:45:06 -0700

    hecto ch.4, setting up files and display logic for text

--- hecto/src/main.rs
+++ hecto/src/main.rs
@@ -1,10 +1,14 @@
 #![warn(clippy::all, clippy::pedantic)]
 mod editor;
 mod terminal;
+mod row;
+mod document;
 
 use editor::Editor;
-pub use terminal::Terminal;
 pub use editor::Position;
+pub use terminal::Terminal;
+pub use document::Document;
+pub use row::Row;
 
 fn main() {
     let mut editor = Editor::default();
